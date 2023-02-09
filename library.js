1e26cffcd3b49e63f9c31839ef4ae8699ccdea5c
JP Niyitanga <jpniyitanga@gmail.com>
2023-02-09 15:53:37 -0500

    Initial commit

--- library.js
+++ library.js
@@ -31,8 +31,12 @@ const library = {
 // p01: Coding Music - 2 tracks
 // p02: Other Playlist - 1 tracks
 const printPlaylists = function() {
-
+     let playlistKeys = Object.keys(library.playlists);
+     for (key of playlistKeys) {
+       console.log(`${key}: ${library.playlists[key].name} - ${library.playlists[key].tracks.length} tracks`);
+     }         
 }
+//printPlaylists();
 
 
 // prints a list of all tracks, using the following format:
@@ -40,23 +44,56 @@ const printPlaylists = function() {
 // t02: Model View Controller by James Dempsey (WWDC 2003)
 // t03: Four Thirty-Three by John Cage (Woodstock 1952)
 const printTracks = function() {
-
+       let tracksKeys = Object.keys(library.tracks);
+       for (key of tracksKeys) {
+              console.log(`${key}: ${library.tracks[key].name} by ${library.tracks[key].artist} (${library.tracks[key].album})`)
+       }
 }
+//printTracks();
 
 
 // prints a list of tracks for a given playlist, using the following format:
 // p01: Coding Music - 2 tracks
 // t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
 // t02: Model View Controller by James Dempsey (WWDC 2003)
-const printPlaylist = function(playlistId) {
+const printPlaylist = function(playlistId) {       
+       playlistNames = Object.keys(library.playlists);
+       for (playlist of playlistNames){
+              if (playlist === playlistId) {
+                     let playlistTracks = Object.values(library.playlists[playlist].tracks);
+                     //console.log(playlistTracks)
+                     console.log(`${playlist}: ${library.playlists[playlist].name} - ${library.playlists[playlist].tracks.length} tracks`);
+
+                     for(track of playlistTracks) {
+                            console.log(`${track}: ${library.tracks[track].name} by ${library.tracks[track].artist} (${library.tracks[track].album})`)
+                     }
+
+              }
+              
+       }
+
 
 }
+//printPlaylist("p02");
 
 
 // adds an existing track to an existing playlist
-const addTrackToPlaylist = function(trackId, playlistId) {
+const addTrackToPlaylist = function(trackId, playlistId) {       
+       let trackKeys = Object.keys(library.tracks);
+                                 
+       if (trackId in library.tracks && playlistId in library.playlists) {
+              //console.log(library.tracks)
+              //console.log(library.playlists)
+              library.playlists[playlistId].tracks.push(trackId);
+       } else {
+              console.log(`Track or Library not found`)
+       }
+       return library.playlists;                                
 
-}
+};
+//console.log(addTrackToPlaylist("t01", "p02"));
+
+
 
 
 // generates a unique id
@@ -64,18 +101,34 @@ const addTrackToPlaylist = function(trackId, playlistId) {
 const generateUid = function() {
   return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
 }
+//console.log(generateUid());
 
 
 // adds a track to the library
 const addTrack = function(name, artist, album) {
+       let newTrack = generateUid();
+       library.tracks[newTrack] = {
+         //id: generateUid(),
+         name: name,
+         artist: artist,
+         album: album
+       };
+       return library.tracks;
+     };
 
-}
+//console.log(addTrack("As It Was", "Harry Styles", "Harry's House"));
 
 
 // adds a playlist to the library
 const addPlaylist = function(name) {
-
-}
+       let newPlayList = generateUid();
+       library.playlists[newPlayList] = {
+              //id: generateUid(),
+              name: name
+       };
+       return library.playlists;
+};
+//console.log(addPlaylist("p5"));
 
 
 // STRETCH:
